01afcb1bdf9d1871d9be6b46ecd7f30708abc962
Adham - TheCryptoChad <adham_elneser@hotmail.com>
2024-10-29 17:24:24 -0500

    responsive service display

--- components/ServiceDisplay.tsx
+++ components/ServiceDisplay.tsx
@@ -75,12 +75,12 @@ export default function ServiceDisplay() {
 	const [selectedStep, setSelectedStep] = useState<Step>('Step Name');
 
 	return (
-		<div className='flex w-full items-center justify-center gap-5 h-[60vh] z-20'>
-			<div className='flex w-1/12 flex-col items-center justify-around h-full'>
+		<div className='z-20 flex w-full items-center justify-center gap-10 max-lg:flex-col lg:h-[60vh] lg:gap-5'>
+			<div className='flex w-full items-center justify-around lg:h-full lg:w-1/12 lg:flex-col'>
 				{Object.keys(serviceSteps).map((service) => (
 					<Button
 						key={service}
-						className={`${selectedService === service ? 'bg-header-button-gradient text-black' : 'bg-[#3C3C3C]'} hover:bg-header-button-gradient border rotate-90 rounded-none font-extrabold`}
+						className={`${selectedService === service ? 'bg-header-button-gradient text-black' : 'bg-[#3C3C3C]'} hover:bg-header-button-gradient rounded-none font-extrabold max-lg:w-1/4 lg:rotate-90 lg:border`}
 						onClick={() => {
 							setSelectedService(service as Service);
 							setSelectedStep(Object.keys(serviceSteps[service as Service])[0] as Step);
@@ -91,17 +91,19 @@ export default function ServiceDisplay() {
 				))}
 			</div>
 
-			<div className='flex w-3/12 flex-col items-center justify-between h-full'>
+			<div className='grid w-full grid-cols-2 max-lg:gap-5 lg:flex lg:h-full lg:w-3/12 lg:flex-col lg:items-center lg:justify-between'>
 				{Object.entries(serviceSteps[selectedService]).map(([key, value]) => (
 					<div
 						key={key}
-						className='flex cursor-pointer gap-5 items-center flex-1 justify-between'
+						className='flex flex-1 cursor-pointer items-center justify-between max-lg:flex-col-reverse lg:gap-5'
 						onClick={() => setSelectedStep(key as Step)}
 					>
-						<h2 className={`${key === selectedStep && 'underline'} font-extrabold text-xl`}>{key}</h2>
+						<h2 className={`${key === selectedStep && 'max-lg:text-[#F7D7A7] lg:underline'} text-xl font-extrabold`}>
+							{key}
+						</h2>
 						<Image
 							alt={key}
-                            className='h-[70%] rounded-lg w-1/2 z-10'
+							className='z-10 h-full w-3/4 rounded-lg lg:h-[70%] lg:w-1/2'
 							height={80}
 							src={value.image}
 							width={80}
@@ -109,15 +111,20 @@ export default function ServiceDisplay() {
 					</div>
 				))}
 			</div>
-			<div className='relative w-2/3 h-full flex flex-col justify-end gap-5 pb-12 px-16'>
+
+			<div className='relative flex h-[120%] w-[90%] flex-col justify-end gap-2 px-4 pb-2 pt-28 lg:h-full lg:w-2/3 lg:gap-5 lg:px-16 lg:pb-12'>
 				<Image
 					alt={selectedStep}
-					className='absolute size-[90%] top-[5%] left-[5%] rounded-xl -z-10'
+					className='absolute left-0 top-0 -z-10 size-full rounded-xl lg:left-[5%] lg:top-[5%] lg:size-[90%]'
 					height={200}
 					src={serviceSteps[selectedService][selectedStep].image}
 					width={200}
 				/>
-				<h2 className='font-extrabold text-3xl'><span className='text-[#F7D7A7]'>01. </span>{serviceSteps[selectedService][selectedStep].title}</h2>
+
+				<h2 className='text-2xl font-extrabold lg:text-3xl'>
+					<span className='text-[#F7D7A7]'>01. </span>
+					{serviceSteps[selectedService][selectedStep].title}
+				</h2>
 				<p className='text-justify'>{serviceSteps[selectedService][selectedStep].description}</p>
 			</div>
 		</div>
