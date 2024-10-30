e2891ce2f3d6efc4025047bc74f416fd25e03f8d
Adham - TheCryptoChad <adham_elneser@hotmail.com>
2024-10-29 19:26:48 -0500

    responsive contact  us

--- app/(routes)/contact-us/page.tsx
+++ app/(routes)/contact-us/page.tsx
@@ -1,67 +1,72 @@
-import ContactForm from '@/components/ContactForm';
-import { Separator } from '@/components/ui/separator';
-import { companyName, phoneNumber, email, address, socials } from '@/lib/constants';
 import Link from 'next/link';
 import Image from 'next/image';
+import ContactForm from '@/components/ContactForm';
+import { phoneNumber, email, address } from '@/lib/constants';
 
 export default function Page() {
 	return (
 		<section className='flex flex-col items-center justify-center gap-16 py-20'>
-			<div className='flex items-center justify-evenly w-4/5'>
-				<div className='flex flex-col justify-between h-full gap-8'>
+			<div className='flex w-4/5 items-center justify-evenly max-lg:flex-col max-lg:gap-8'>
+				<div className='flex h-full flex-col justify-between gap-8 max-lg:w-full'>
 					<h1 className='text-4xl font-extrabold text-black'>Contact Us</h1>
 
 					<div className='flex flex-col justify-between gap-4 text-[#54595F]'>
-						<div className='flex items-center gap-5 hover:text-black'>
-							<Image
-								alt='Phone'
-								className='filter invert'
-								height={30}
-								src='/icons/phone2.svg'
-								width={30}
-							/>
-							<Link href={`tel:${phoneNumber}`}>
-								<h2 className='text-3xl font-extrabold'>{phoneNumber}</h2>
-							</Link>
-						</div>
-						<div className='flex items-center gap-5 hover:text-black'>
-							<Image
-								alt='Email'
-								className='filter invert'
-								height={30}
-								src='/icons/email.svg'
-								width={30}
-							/>
-							<Link href={`mailto:${email}`}>
-								<h2 className='text-xl underline'>{email}</h2>
-							</Link>
-						</div>
-						<div className='flex items-center gap-5 hover:text-black'>
-							<Image
-								alt='Location'
-								className='filter invert'
-								height={30}
-								src='/icons/location.svg'
-								width={30}
-							/>
-							<h2 className='text-xl'>{address}</h2>
-						</div>
+						<Socials name='Phone' />
+						<Socials name='Email' />
+						<Socials name='Location' />
 					</div>
 
-					<Image alt='Google Review' height={80} src='/images/google-review.webp' width={200} />
+					<Image
+						alt='Google Review'
+						height={80}
+						src='/images/google-review.webp'
+						width={200}
+					/>
 				</div>
 
 				<ContactForm />
 			</div>
 
-			<div className="bg-white relative w-[65%] h-[80vh]">
-					<iframe 
-						className="border-none relative size-full z-20"
-						loading="lazy"  
-						src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=New%20York%2C%20wendys&zoom=10&maptype=roadmap">
-					</iframe>
-					<a className='text-black absolute left-0 top-0 z-0' href="https://www.zrivo.com/nebraska-paycheck-calculator">Nebraska Paycheck Calculator</a>
-				</div>
+			<div className='relative h-[80vh] w-[65%] bg-white max-lg:h-[50vh] max-lg:w-[90%]'>
+				<iframe
+					className='relative z-20 size-full border-none'
+					loading='lazy'
+					src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=New%20York%2C%20wendys&zoom=10&maptype=roadmap'
+				></iframe>
+				<a
+					className='absolute left-0 top-0 z-0 text-black'
+					href='https://www.zrivo.com/nebraska-paycheck-calculator'
+				>
+					Nebraska Paycheck Calculator
+				</a>
+			</div>
 		</section>
+	);
+}
+
+type SProps = {
+	name: string;
+};
+
+function Socials(props: SProps) {
+	return (
+		<div className='flex items-center gap-5 hover:text-black'>
+			<Image
+				alt={props.name}
+				className='invert filter'
+				height={30}
+				src={`/icons/${props.name === 'Phone' ? 'phone2' : props.name.toLowerCase()}.svg`}
+				width={30}
+			/>
+			{props.name === 'Location' ? (
+				<h2 className='text-xl'>{address}</h2>
+			) : (
+				<Link href={props.name === 'Phone' ? `tel:${phoneNumber}` : `mailto:${email}`}>
+					<h2 className={`${props.name === 'Phone' ? 'text-3xl' : 'text-xl underline'} font-extrabold`}>
+						{props.name === 'Phone' ? phoneNumber : email}
+					</h2>
+				</Link>
+			)}
+		</div>
 	);
 }
