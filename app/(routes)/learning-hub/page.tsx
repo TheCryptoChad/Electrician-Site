7b69dc2b910358000a3b2ec60281b1d56cbee564
Adham - TheCryptoChad <adham_elneser@hotmail.com>
2024-10-29 19:11:46 -0500

    responsive learning

--- app/(routes)/learning-hub/page.tsx
+++ app/(routes)/learning-hub/page.tsx
@@ -1,26 +1,23 @@
-import ContactForm from '@/components/ContactForm';
-import { Separator } from '@/components/ui/separator';
-import { companyName, phoneNumber, email, address, socials } from '@/lib/constants';
 import Link from 'next/link';
-import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { socials } from '@/lib/constants';
 
 export default function Page() {
 	return (
-		<section className='flex flex-col items-center justify-center py-20 gap-10'>
+		<section className='flex flex-col items-center justify-center gap-10 py-20'>
 			<div className='flex w-4/5 flex-col gap-4'>
 				<h1 className='text-4xl'>Why we create content?</h1>
 				<p className='text-[#7a7a7a]'>
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum delectus, provident reprehenderit ducimus ex
 					rerum commodi inventore ut, perspiciatis error dolores sint dicta! Animi, voluptate repellat. Dolorum saepe
-					iusto labore? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum delectus, provident reprehenderit ducimus ex
-					rerum commodi inventore ut, perspiciatis error dolores sint dicta! Animi, voluptate repellat. Dolorum saepe
-					iusto labore? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum delectus, provident reprehenderit ducimus ex
-					rerum commodi inventore ut, perspiciatis error dolores sint dicta! Animi, voluptate repellat. Dolorum saepe
-					iusto labore?
+					iusto labore? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum delectus, provident
+					reprehenderit ducimus ex rerum commodi inventore ut, perspiciatis error dolores sint dicta! Animi, voluptate
+					repellat. Dolorum saepe iusto labore? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+					delectus, provident reprehenderit ducimus ex rerum commodi inventore ut, perspiciatis error dolores sint
+					dicta! Animi, voluptate repellat. Dolorum saepe iusto labore?
 				</p>
 			</div>
-			
+
 			<div className='w-4/5'>
 				<script
 					src='https://static.elfsight.com/platform/platform.js'
@@ -33,14 +30,14 @@ export default function Page() {
 			</div>
 
 			<Link
-					className='flex w-full justify-center'
-					href={socials.Youtube}
-					target='_blank'
-				>
-					<Button className='bg-header-button-gradient h-12 w-[20%] text-black rounded-lg text-lg font-bold transition-transform duration-300 hover:scale-110'>
-						Join The Community!
-					</Button>
-				</Link>
+				className='flex w-full justify-center'
+				href={socials.Youtube}
+				target='_blank'
+			>
+				<Button className='bg-header-button-gradient h-12 w-2/3 rounded-lg text-lg font-bold text-black transition-transform duration-300 hover:scale-110 lg:w-[20%]'>
+					Join The Community!
+				</Button>
+			</Link>
 		</section>
 	);
 }
